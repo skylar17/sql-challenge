67524f377d34c66405faa22df09c2e0e93e52935
skylar17 <64751046+skylar17@users.noreply.github.com>
2020-07-20 16:01:18 -0400

    added Epilogue

--- EmployeeSQL/3.Data Analysis/analysis_queries.sql
+++ EmployeeSQL/3.Data Analysis/analysis_queries.sql
@@ -53,3 +53,13 @@ SELECT last_name, COUNT(*) AS freq_count
 FROM employees
 GROUP BY last_name
 ORDER BY freq_count DESC;
+
+
+
+-- Epilogue 
+--(Evidence in hand, you march into your boss's office and present the visualization. With a sly grin, \
+-- your boss thanks you for your work. On your way out of the office, you hear the words, \
+-- "Search your ID number." You look down at your badge to see that your employee ID number is 499942.)
+SELECT *
+FROM employees
+WHERE emp_no = 499942;
